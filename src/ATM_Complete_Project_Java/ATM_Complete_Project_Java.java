5e06053767cb7d037c353d7c4522d1959e76baca
Mithursan Kalanantharasan <mitho9798@gmail.com>
2024-08-20 23:12:35 +0530

    undo

--- src/ATM_Complete_Project_Java/ATM_Complete_Project_Java.java
+++ src/ATM_Complete_Project_Java/ATM_Complete_Project_Java.java
@@ -6,7 +6,15 @@ public class ATM_Complete_Project_Java {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Mithursan Kalanantharasan");
+		int pin = 1234;
+		int balance = 10000;
+		
+		int addAmount = 0;
+		int takeAmount = 0;
+		
+		string name;
+		
+		Scanner scanner = new 
 		
 
 	}
