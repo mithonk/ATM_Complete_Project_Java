4a1a9eebbf4dd9f282f393a3c6cdd9df588b62df
Mithursan Kalanantharasan <mitho9798@gmail.com>
2024-08-20 23:11:48 +0530

    Test

--- src/ATM_Complete_Project_Java/ATM_Complete_Project_Java.java
+++ src/ATM_Complete_Project_Java/ATM_Complete_Project_Java.java
@@ -6,15 +6,7 @@ public class ATM_Complete_Project_Java {
 
 	public static void main(String[] args) {
 		
-		int pin = 1234;
-		int balance = 10000;
-		
-		int addAmount = 0;
-		int takeAmount = 0;
-		
-		string name;
-		
-		Scanner scanner = new 
+		System.out.println("Mithursan Kalanantharasan");
 		
 
 	}
